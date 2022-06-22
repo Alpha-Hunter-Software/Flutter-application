3d310c3bab5a9ddea4df442dab664b09f1988db9
Kadriye <kadriyemacit9655@gmail.com>
2022-06-22 21:58:27 +0300

    Fix berkay's homework

--- flutter_application/lib/homeworks/berkay/new_file/home_screen.dart
+++ flutter_application/lib/homeworks/berkay/new_file/home_screen.dart
@@ -12,41 +12,40 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text("Hunter Season"),
-        ),
-        drawer: Drawer(
-          child: ListView(
-            padding: EdgeInsets.zero,
-            children: [
-              UserAccountsDrawerHeader(
-                accountName: Text("Berkay"),
-                accountEmail: Text("Berkaycobanbas@gmail.com"),
-                currentAccountPicture: CircleAvatar(),
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Hunter Season"),
+      ),
+      drawer: Drawer(
+        child: ListView(
+          padding: EdgeInsets.zero,
+          children: [
+            UserAccountsDrawerHeader(
+              accountName: Text("Berkay"),
+              accountEmail: Text("Berkaycobanbas@gmail.com"),
+              currentAccountPicture: CircleAvatar(
+                backgroundImage: AssetImage("assets/images/homeperson.png"),
               ),
-              Divider(
-                color: Colors.black,
-              ), //çizgi
-              ListTile(
-                title: Text("Profil"), //ana yazı
-                subtitle: Text("Profile gider"), //alt yazı
-                leading: Icon(Icons.person), //başına
-                trailing: Icon(Icons.arrow_forward_ios), //sonuna
-              ),
-              Divider(
-                color: Colors.black,
-              ), //çizgi
-              ListTile(
-                title: Text("Ayarlar"), //ana yazı
-                subtitle: Text("Ayarlara gider"), //alt yazı
-                leading: Icon(Icons.settings), //başına
-                trailing: Icon(Icons.arrow_forward_ios), //sonuna
-              ),
-            ],
-          ),
+            ),
+            Divider(
+              color: Colors.black,
+            ), //çizgi
+            ListTile(
+              title: Text("Profil"), //ana yazı
+              subtitle: Text("Profile gider"), //alt yazı
+              leading: Icon(Icons.person), //başına
+              trailing: Icon(Icons.arrow_forward_ios), //sonuna
+            ),
+            Divider(
+              color: Colors.black,
+            ), //çizgi
+            ListTile(
+              title: Text("Ayarlar"), //ana yazı
+              subtitle: Text("Ayarlara gider"), //alt yazı
+              leading: Icon(Icons.settings), //başına
+              trailing: Icon(Icons.arrow_forward_ios), //sonuna
+            ),
+          ],
         ),
       ),
     );
--- flutter_application/lib/homeworks/berkay/new_file/register_screen.dart
+++ flutter_application/lib/homeworks/berkay/new_file/register_screen.dart
@@ -20,129 +20,133 @@ class _RegisterScreenState extends State<RegisterScreen> {
 //
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.all(10.0),
-      child: SingleChildScrollView(
-        child: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: [
-              //  SizedBox(height: 50, width: 15),
-              // Image.asset("assets/images/anime.png", height: 200, width: 150),
-              SizedBox(
-                height: 20,
-                width: 20,
-              ),
-              Column(
+    return Scaffold(
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.all(10.0),
+          child: SingleChildScrollView(
+            child: Center(
+              child: Column(
                 mainAxisAlignment: MainAxisAlignment.center,
-                crossAxisAlignment: CrossAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.stretch,
                 children: [
-                  Container(
-                    decoration: BoxDecoration(
-                        color: Colors.white,
-                        borderRadius: BorderRadius.circular(15),
-                        boxShadow: [BoxShadow(color: Colors.black)]),
-                    height: 100,
-                    width: 400,
-                    child: TextField(
-                      controller: textEditingControllerUSE,
-                      keyboardType: TextInputType.emailAddress,
-                      style: TextStyle(
-                        color: Colors.red.shade900,
-                        fontWeight: FontWeight.w900,
-                      ),
-                      cursorColor: Colors.red,
-                      decoration: InputDecoration(
-                          border: OutlineInputBorder(
-                              borderRadius: BorderRadius.circular(20),
-                              gapPadding: 1),
-                          helperText: "Username."),
-                      //
-                    ),
+                  //  SizedBox(height: 50, width: 15),
+                  // Image.asset("assets/images/anime.png", height: 200, width: 150),
+                  SizedBox(
+                    height: 20,
+                    width: 20,
                   ),
-                  Container(
-                    decoration: BoxDecoration(
-                        color: Colors.white,
-                        borderRadius: BorderRadius.circular(15),
-                        boxShadow: [BoxShadow(color: Colors.black)]),
-                    height: 100,
-                    width: 400,
-                    child: TextField(
-                      controller: textEditingControllerEMAIL,
-                      keyboardType: TextInputType.emailAddress,
-                      style: TextStyle(
-                        color: Colors.red.shade900,
-                        fontWeight: FontWeight.w900,
-                      ),
-                      cursorColor: Colors.red,
-                      decoration: InputDecoration(
-                          border: OutlineInputBorder(
-                              borderRadius: BorderRadius.circular(20),
-                              gapPadding: 1),
-                          helperText: "Email "),
-                      //
-                    ),
-                  ),
-                  Container(
-                    decoration: BoxDecoration(
-                        color: Colors.white,
-                        borderRadius: BorderRadius.circular(15),
-                        boxShadow: [BoxShadow(color: Colors.black)]),
-                    height: 100,
-                    width: 400,
-                    child: TextField(
-                      controller: textEditingControllerPWW,
-                      keyboardType: TextInputType.emailAddress,
-                      style: TextStyle(
-                        color: Colors.red.shade900,
-                        fontWeight: FontWeight.w900,
-                      ),
-                      cursorColor: Colors.red,
-                      obscureText: true,
-                      decoration: InputDecoration(
-                        border: OutlineInputBorder(
-                            borderRadius: BorderRadius.circular(20),
-                            gapPadding: 1),
-                        helperText: "Password",
-                      ),
-                      //
-                    ),
-                  ),
-                  const Divider(
-                    color: Colors.blue,
-                  ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
                     crossAxisAlignment: CrossAxisAlignment.center,
                     children: [
-                      ElevatedButton(
-                        onPressed: () {
-                          Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                              builder: (context) => HomeScreen(),
-                            ),
-                          );
-                        },
-                        child: const Text("Register"),
+                      Container(
+                        decoration: BoxDecoration(
+                            color: Colors.white,
+                            borderRadius: BorderRadius.circular(15),
+                            boxShadow: [BoxShadow(color: Colors.black)]),
+                        height: 100,
+                        width: 400,
+                        child: TextField(
+                          controller: textEditingControllerUSE,
+                          keyboardType: TextInputType.emailAddress,
+                          style: TextStyle(
+                            color: Colors.red.shade900,
+                            fontWeight: FontWeight.w900,
+                          ),
+                          cursorColor: Colors.red,
+                          decoration: InputDecoration(
+                              border: OutlineInputBorder(
+                                  borderRadius: BorderRadius.circular(20),
+                                  gapPadding: 1),
+                              helperText: "Username."),
+                          //
+                        ),
                       ),
-                      ElevatedButton(
-                        onPressed: () {
-                          Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                              builder: (context) => OkeyProfil(),
-                            ),
-                          );
-                        },
-                        child: const Text("Kayıt Ol"),
+                      Container(
+                        decoration: BoxDecoration(
+                            color: Colors.white,
+                            borderRadius: BorderRadius.circular(15),
+                            boxShadow: [BoxShadow(color: Colors.black)]),
+                        height: 100,
+                        width: 400,
+                        child: TextField(
+                          controller: textEditingControllerEMAIL,
+                          keyboardType: TextInputType.emailAddress,
+                          style: TextStyle(
+                            color: Colors.red.shade900,
+                            fontWeight: FontWeight.w900,
+                          ),
+                          cursorColor: Colors.red,
+                          decoration: InputDecoration(
+                              border: OutlineInputBorder(
+                                  borderRadius: BorderRadius.circular(20),
+                                  gapPadding: 1),
+                              helperText: "Email "),
+                          //
+                        ),
                       ),
+                      Container(
+                        decoration: BoxDecoration(
+                            color: Colors.white,
+                            borderRadius: BorderRadius.circular(15),
+                            boxShadow: [BoxShadow(color: Colors.black)]),
+                        height: 100,
+                        width: 400,
+                        child: TextField(
+                          controller: textEditingControllerPWW,
+                          keyboardType: TextInputType.emailAddress,
+                          style: TextStyle(
+                            color: Colors.red.shade900,
+                            fontWeight: FontWeight.w900,
+                          ),
+                          cursorColor: Colors.red,
+                          obscureText: true,
+                          decoration: InputDecoration(
+                            border: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(20),
+                                gapPadding: 1),
+                            helperText: "Password",
+                          ),
+                          //
+                        ),
+                      ),
+                      const Divider(
+                        color: Colors.blue,
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: [
+                          ElevatedButton(
+                            onPressed: () {
+                              Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                  builder: (context) => HomeScreen(),
+                                ),
+                              );
+                            },
+                            child: const Text("Register"),
+                          ),
+                          ElevatedButton(
+                            onPressed: () {
+                              Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                  builder: (context) => OkeyProfil(),
+                                ),
+                              );
+                            },
+                            child: const Text("Kayıt Ol"),
+                          ),
+                        ],
+                      )
                     ],
                   )
                 ],
-              )
-            ],
+              ),
+            ),
           ),
         ),
       ),
